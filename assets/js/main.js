5a666101b2f5f7571f124addfe12dee8395de496
AmanSalman <rapamansalman@gmail.com>
2024-10-21 23:37:07 +0300

    navigation bar, photo section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ const moviesList = document.getElementById('movies-list');
 async function fetchTopRatedMovies() {
     const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`);
     const data = await response.json();
+    console.log(data.results)
     displayMovies(data.results);
 }
 
@@ -18,6 +19,7 @@ function displayMovies(movies) {
             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
             <div class="overlay">
                 <h3>${movie.title}</h3>
+                <span>${movie.vote_average} ⭐ </span> 
                 <button onclick="viewDetails(${movie.id})">Details</button>
             </div>
         `;
@@ -25,36 +27,6 @@ function displayMovies(movies) {
     });
 }
 
-// Navigate to movie details
-function viewDetails(movieId) {
-    window.location.href = `movie-details.html?id=${movieId}`;
-}
 
 // Initialize the app
 fetchTopRatedMovies();
-
-
-// Save favorite movie
-function saveFavorite(movie) {
-  let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  favorites.push(movie);
-  localStorage.setItem('favorites', JSON.stringify(favorites));
-}
-
-// Fetch favorites
-function fetchFavorites() {
-  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-  const favoritesList = document.getElementById('favorites-list');
-  favorites.forEach(movie => {
-      const card = document.createElement('div');
-      card.className = 'movie-card';
-      card.innerHTML = `
-          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-          <h3>${movie.title}</h3>
-          <button onclick="removeFavorite(${movie.id})">Remove</button>
-      `;
-      favoritesList.appendChild(card);
-  });
-}
-
-// Call fetchFavorites() on favorites.html
