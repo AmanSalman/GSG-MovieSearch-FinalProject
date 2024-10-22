1c4287ea8b9db3b4b954f2de1b9d16f732bd7ba2
AmanSalman <rapamansalman@gmail.com>
2024-10-22 19:57:12 +0300

    add movies details modifications

---
+++ assets/js/details.js
@@ -0,0 +1,71 @@
+const API_KEY = '4038d7dc8938db59fb12025f3ee4e770';
+const movieheader = document.querySelector('.movie-header');
+const story = document.querySelector('.story');
+const imagegallery = document.querySelector('.image-gallery');
+
+function getMovieIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get('id');
+}
+
+async function fetchMovieDetails() {
+    const movieId = getMovieIdFromUrl(); 
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`);
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const imges = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/images?api_key=${API_KEY}`);
+        const data = await response.json();
+        const imgData = await imges.json();
+
+        displayMovie(data, imgData.backdrops);
+    } catch (error) {
+        // Handle error by displaying a "Not Found" message
+        displayNotFoundMessage(error.message);
+    }
+}
+
+function displayMovie(movie, images) {
+    movieheader.innerHTML = `
+        <div class="movie-poster">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title}">
+        </div>
+        <div class="movie-info">
+            <h1>${movie.original_title}</h1>
+            <p class="tagline">${movie.tagline}</p>
+            <div class="details">
+                <span class="duration">${movie.runtime} Min.</span>
+                ${movie.production_countries.map(m => `<span class="country">${m.name}</span>`).join('')}
+                <span class="release-date">${movie.release_date}</span>
+            </div>
+            <div class="rating-info">
+                <span class="rating-score">${movie.vote_average}</span>
+                <div class="stars">
+                    <span>★★★★★★★★★★</span>
+                    <span class="votes">${movie.vote_count} votes</span>
+                </div>
+            </div>
+            <div class="categories">
+                ${movie.genres.map(g => `<span>${g.name}</span>`).join('')}
+            </div>
+        </div>
+    `;
+
+    story.innerHTML = `${movie.overview}`;
+    imagegallery.innerHTML = images.map((img, index) => {
+        return `<img src="https://image.tmdb.org/t/p/w500${img.file_path}" alt="Scene ${index}">`;
+    }).join('');
+}
+
+// Function to display a "Not Found" message
+function displayNotFoundMessage(errorMessage) {
+    const storysection = document.querySelector('.story-section');
+    movieheader.innerHTML = `<h1>Movie Not Found</h1>`;
+    storysection.innerHTML = ``;
+}
+
+// Initialize the app
+fetchMovieDetails();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,64 @@
-const API_KEY = '4038d7dc8938db59fb12025f3ee4e770';
-const moviesList = document.getElementById('movies-list');
+const API_KEY = "4038d7dc8938db59fb12025f3ee4e770";
+const moviesList = document.getElementById("movies-list");
 
 // Fetch top-rated movies
 async function fetchTopRatedMovies() {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`);
-    const data = await response.json();
-    console.log(data.results)
-    displayMovies(data.results.slice(0,12));
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
+  );
+  const data = await response.json();
+  console.log(data.results);
+  displayMovies(data.results.slice(0, 12));
 }
 
 // Display movies in the UI
 function displayMovies(movies) {
-    moviesList.innerHTML = '';
-    movies.forEach(movie => {
-        const card = document.createElement('div');
-        card.className = 'movie-card';
-        card.innerHTML = `
+  moviesList.innerHTML = "";
+  movies.forEach((movie) => {
+    const card = document.createElement("div");
+    card.className = "movie-card";
+    card.innerHTML = `
             <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
             <div class="overlay">
                 <h3>${movie.title}</h3>
                 <span>${movie.vote_average} ⭐ </span> 
                 <span class="fav">♡</span>
-                <button onclick="viewDetails(${movie.id})">Details</button>
-            </div>
+                <a href="details.html?id=${movie.id}" class="button-details">View Details</a>            </div>
         `;
-        moviesList.appendChild(card);
-    });
+    moviesList.appendChild(card);
+  });
 }
-
 
 // Initialize the app
 fetchTopRatedMovies();
 
-
-
 function getQueryParam(param) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
 }
 
 async function fetchMovies(query) {
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data = await response.json();
-    return data.results.length > 0 ? data.results : [];
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results.length > 0 ? data.results : [];
 }
 
 async function displaySearchResults() {
-    const query = getQueryParam('query');
-    if (query) {
-        const movies = await fetchMovies(query);
-        moviesList.innerHTML = '';
+  const query = getQueryParam("query");
+  if (query) {
+    const movies = await fetchMovies(query);
+    moviesList.innerHTML = "";
 
-        if (movies.length > 0) {
-            movies.forEach(movie => {
-                if (movie.poster_path) {
-                    const card = document.createElement('div');
-                    card.className = 'movie-card';
-                    card.innerHTML = `
+    if (movies.length > 0) {
+      movies.forEach((movie) => {
+        if (movie.poster_path) {
+          const card = document.createElement("div");
+          card.className = "movie-card";
+          card.innerHTML = `
                         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
                         <div class="overlay">
                             <h3>${movie.title}</h3>
@@ -65,29 +67,32 @@ async function displaySearchResults() {
                             <button onclick="viewDetails(${movie.id})">Details</button>
                         </div>
                     `;
-                    moviesList.appendChild(card);
-                }
-            });
-
-            if (moviesList.innerHTML === '') {
-                moviesList.innerHTML = '<p>No movies found with images for this search.</p>';
-            }
-        } else {
-            moviesList.innerHTML = '<p>No movies found for this search.</p>';
+          moviesList.appendChild(card);
         }
+      });
+
+      if (moviesList.innerHTML === "") {
+        moviesList.innerHTML =
+          "<p>No movies found with images for this search.</p>";
+      }
+    } else {
+      moviesList.innerHTML = "<p>No movies found for this search.</p>";
     }
+  }
 }
 
-if (window.location.pathname.includes('search-results.html')) {
-    displaySearchResults();
+if (window.location.pathname.includes("search-results.html")) {
+  displaySearchResults();
 }
 
-const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search');
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search");
 
-searchButton.addEventListener('click', () => {
-    const query = searchInput.value.trim();
-    if (query) {
-        window.location.href = `search-results.html?query=${encodeURIComponent(query)}`;
-    }
+searchButton.addEventListener("click", () => {
+  const query = searchInput.value.trim();
+  if (query) {
+    window.location.href = `search-results.html?query=${encodeURIComponent(
+      query
+    )}`;
+  }
 });
