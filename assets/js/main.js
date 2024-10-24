95bda0560ca30ce290b71a4072738c523330451c
AmanSalman <rapamansalman@gmail.com>
2024-10-24 17:32:31 +0300

    changes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,27 +7,29 @@ async function fetchTopRatedMovies() {
     `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`
   );
   const data = await response.json();
-  localStorage.setItem('TopRated', JSON.stringify(data.results.slice(0, 12)));
   displayMovies(data.results.slice(0, 12));
+  // localStorage.setItem('TopRated', JSON.stringify(data.results.slice(0, 12)));
 }
 
 function displayMovies(movies) {
   moviesList.innerHTML = "";
   movies.forEach((movie) => {
-    const card = document.createElement("div");
-    card.className = "movie-card";
-    const isFavorite = isMovieFavorite(movie.id) ? "♥" : "♡";
-
-    card.innerHTML = `
-      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-      <div class="overlay">
-        <h3>${movie.title}</h3>
-        <span>${movie.vote_average} ⭐</span> 
-        <span class="fav" data-id="${movie.id}" onclick="toggleFavorite(${movie.id})">${isFavorite}</span>
-        <a href="details.html?id=${movie.id}" class="button-details">View Details</a>
-      </div>
-    `;
-    moviesList.appendChild(card);
+    if(movie.poster_path){
+      const card = document.createElement("div");
+      card.className = "movie-card";
+      const isFavorite = isMovieFavorite(movie.id) ? "♥" : "♡";
+  
+      card.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+        <div class="overlay">
+          <h3>${movie.title}</h3>
+          <span>${movie.vote_average} ⭐</span> 
+          <span class="fav" data-id="${movie.id}" onclick="toggleFavorite(${movie.id})">${isFavorite}</span>
+          <a href="details.html?id=${movie.id}" class="button-details">View Details</a>
+        </div>
+      `;
+      moviesList.appendChild(card);
+    }
   });
 }
 
@@ -39,7 +41,7 @@ function toggleFavorite(movieId) {
     favorites.push(movieId);
   }
   localStorage.setItem("favorites", JSON.stringify(favorites));
-  fetchTopRatedMovies();
+  window.location = '../../favorites.html'
 }
 
 function getFavorites() {
